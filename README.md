9b2a0a7b03d63fc42c8b9565cd8626e4a90240d9
sdfunes <sdfunes@gmail.com>
2025-02-26 21:12:32 -0300

    feat: Agrego preguntas frecuentes al index

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ index.html es la página principal de la aplicación sin estar "logueado"
 home.html es la página inicial cuando un usuario se "loguea"
 
 https://yendoo-coderhouse.vercel.app/
+
+- para el curso de Javascript Flex agregué el include al js en el index.html y por el momento es la única página que tiene interacción.
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   console.log('Página cargada completamente');
-  suscribirseNewsletter();
+  suscribirseAlNewsletter();
 };
 
 function descargarApp() {
@@ -35,11 +35,37 @@ function descargarApp() {
   }
 }
 
-function suscribirseNewsletter() {
+function suscribirseAlNewsletter() {
   if (confirm('¿Desea suscribirse a nuestro newsletter?')) {
     let email = prompt('Ingrese su email');
     if (email) {
       alert('Gracias por suscribirse a nuestro newsletter');
     }
   }
+}
+
+function mostrarPreguntasFrecuentes() {
+  let preguntas = [
+    {
+      pregunta: '¿Cómo me registro a la APP?',
+      respuesta:
+        'Para registrarte en la APP debes dirigirte a la opción de "Registrarse" y allí podrás completar tus datos para registrarte.',
+    },
+    {
+      pregunta: '¿Cómo ingreso a la aplicación?',
+      respuesta:
+        'Para ingresar a la aplicación debes dirigirte a la opción de "Login" e ingresar tu usuario y contraseña con los que te registraste.',
+    },
+    {
+      pregunta: '¿Cómo cargo kilómetros?',
+      respuesta:
+        'Para cargar kilómetros debes dirigirte a la opción de "Cargar Kilómetros", ingresar la cantidad y seleccionar un modo de pago.',
+    },
+  ];
+  let html = 'PREGUNTAS FRECUENTES </br>';
+  for (let i = 0; i < preguntas.length; i++) {
+    html += '<h3>' + preguntas[i].pregunta + '</h3>';
+    html += '<p>' + preguntas[i].respuesta + '</p></br>';
+  }
+  alert(html);
 }
